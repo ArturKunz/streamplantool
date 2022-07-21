53d69c767434511b9839dd5582241e05fa199f61
Aitur <artur.kunz@ruhr-uni-bochum.de>
2022-07-21 19:10:46 +0200

    Pydantic Validation

--- read_json.py
+++ read_json.py
@@ -1,7 +1,10 @@
 import json
 from pathlib import Path
 
+from pydantic import validate_arguments
 
+
+@validate_arguments
 def read_json(path: Path = Path("data/data.json")):
     json_file = path.read_text()
     data_dict = json.loads(json_file)
